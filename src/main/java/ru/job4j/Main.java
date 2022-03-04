c9a1699532e99eb11a3b9aacd0d6a89ef8b2142f
tinnikov <tinnikovkr@gmail.com>
2022-03-04 22:05:37 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,7 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        byte age = 127;
+        System.out.println(age);
     }
 }
