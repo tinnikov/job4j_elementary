c368d090746d5b3a0b4df00925b3334a59281974
tinnikov <tinnikovkr@gmail.com>
2022-03-07 13:35:18 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,11 +1,12 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value){
+    public static float rubleToEuro(float value) {
         float result = value / 70;
         return result;
     }
-    public static float rubleToDollar(float value){
+
+    public static float rubleToDollar(float value) {
         float result = value / 60;
         return result;
     }
