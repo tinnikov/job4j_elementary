ba063937f3cb9982387e0b499ee3384f2cc49644
tinnikov <tinnikovkr@gmail.com>
2022-03-11 20:35:42 +0300

    1.2. Входные данные в тесте [#297750 #273851]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -19,7 +19,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then40() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -30,7 +30,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then40() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -41,7 +41,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1Then40() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -52,7 +52,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X0Then40() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
